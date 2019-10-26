8ae2d1cb3b13e500d8d1fa9c896b8d4ea2303247
Samuel J Wilkins <wilkins.samuelj@gmail.com>
2019-10-26 05:40:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,21 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  newArray = [element, ...array]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  newArray = [...array, element]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.push(element)
+  return array
+}
