55dabeb3d6436344c18765d1cbc1d4883a2269ba
Samuel J Wilkins <wilkins.samuelj@gmail.com>
2019-10-26 07:33:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,7 @@ function addElementToEndOfArray(array, element) {
   return newArray
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
+function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
 }
